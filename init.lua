0729f865a8ca82f79704a025752b9ad93dce885e
Markus Svedberg <markus.svedberg@saabgroup.com>
2025-04-22 13:23:39 +0200

    Updated telescope behavior

--- init.lua
+++ init.lua
@@ -323,11 +323,12 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<enter>'] = 'file_tab' },
+            n = { ['<enter>'] = 'file_tab' },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,9 +1,11 @@
 local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find files in project' })
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Find files in git repository' })
+
 vim.keymap.set('n', '<leader>ff', function()
-  builtin.find_files({ hidden = true }) 
+  builtin.find_files { hidden = true }
 end, { desc = 'Find files in project (including hidden)' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find files in git repository' })
+
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string { search = vim.fn.input 'Grep > ' }
 end, { desc = 'Grep search in project' })
