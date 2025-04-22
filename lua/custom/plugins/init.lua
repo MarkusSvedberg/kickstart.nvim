7004626435a26e339d585cbbae370b20dd58f4cb
Markus Svedberg <markus.svedberg@saabgroup.com>
2025-04-22 10:26:45 +0200

    Improved automatic cd on start, added find hidden files key

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,26 +4,27 @@
 -- See the kickstart.nvim README for more information
 
 -- Stop netrw from loading
-
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 
-function file_exists(name)
-  local f = io.open(name, 'r')
-  return f ~= nil and io.close(f)
-end
-
-function get_path(str)
+-- Change path to current opened file or directory
+local function get_path(str)
   return str:match '(.*[/\\])'
 end
 
 if #vim.v.argv > 2 then
-  path = vim.v.argv[3]
+  local path = vim.v.argv[3]
 
-  if not file_exists(path) then
-    vim.fn.chdir(path)
-  else
-    vim.fn.chdir(get_path(path))
+  -- Check if path exists
+  if vim.uv.fs_stat(path) then
+    -- Is it a directory?
+    if vim.fn.isdirectory(path) == 1 then
+      vim.fn.chdir(path)
+    else
+      -- If it is a file, get parent
+      local dirpath = vim.fs.dirname(path)
+      vim.fn.chdir(dirpath)
+    end
   end
 end
 
